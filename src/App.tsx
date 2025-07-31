e9f30bd2e74c944d6433ef1f5efbc6f1ae0a49ea
Andrew Pavlenko <andrew.pawllenko@gmail.com>
2025-07-31 16:59:48 +0300

    complete to do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,79 @@
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import { ErrorMessages } from './components/Error/ErrorMessages';
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { useTodos } from './hooks/useTodos';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 export const App: React.FC = () => {
+  const {
+    errorMessage,
+    setErrorMessage,
+    statusFilter,
+    setStatusFilter,
+    handleHideError,
+    hasTodos,
+    filteredTodos,
+    activeTodos,
+    handleTodoDelete,
+    handleDeleteAllCompletedTodos,
+    allTodosCompleted,
+    isCompletedTodos,
+    handleTodoAdd,
+    tempTodo,
+    setTempTodo,
+    inputFocus,
+    todoInOperation,
+    handleUseToggle,
+    handleTodoStatusToggle,
+    handleTodoTitleUpdate,
+    isLoading,
+  } = useTodos();
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">To do</h1>
+
+      <div className="todoapp__content">
+        <Header
+          allTodosCompleted={allTodosCompleted}
+          handleTodoAdd={handleTodoAdd}
+          setErrorMessage={setErrorMessage}
+          setTempTodo={setTempTodo}
+          inputFocus={inputFocus}
+          onToggle={handleUseToggle}
+          isLoading={isLoading}
+          hasTodos={hasTodos}
+        />
+
+        <TodoList
+          filteredTodos={filteredTodos}
+          handleTodoDelete={handleTodoDelete}
+          tempTodo={tempTodo}
+          todoInOperation={todoInOperation}
+          handleTodoStatusToggle={handleTodoStatusToggle}
+          handleTodoTitleUpdate={handleTodoTitleUpdate}
+        />
+        {hasTodos && (
+          <Footer
+            activeTodos={activeTodos}
+            statusFilter={statusFilter}
+            setStatusFilter={setStatusFilter}
+            isCompletedTodos={isCompletedTodos}
+            handleDeleteAllCompletedTodos={handleDeleteAllCompletedTodos}
+          />
+        )}
+      </div>
+      <ErrorMessages
+        errorMessage={errorMessage}
+        onHideError={handleHideError}
+      />
     </div>
   );
 };
